195bd6b3f1bcbd251d813b2b8c3f7cd016171bfa
Chandru Gopalakrishnan <gchandru1@gmail.com>
2015-03-08 05:23:46 -0400

    Improved searching, saving tweet data to csv

--- mining_tweets.py
+++ mining_tweets.py
@@ -1,5 +1,7 @@
 from twython import Twython
 import config
+import json 
+import csv
 
 def get_access_token():
     f = open('access_token.txt', 'r')
@@ -8,9 +10,33 @@ def get_access_token():
 ACCESS_TOKEN = get_access_token()
 twitter = Twython(config.CONSUMER_KEY, access_token = ACCESS_TOKEN)
 
+def pretty_print(d):
+    print json.dumps(d, indent = 4)
+    
 
 #get tweets for given query
-def search_tweets():
-    return twitter.search(q='thedress', result_type = 'popular')
+def search_tweets(search_term, rtype):
+    query = search_term + ' since:2015-02-26'
+    results = twitter.search(q = query, result_type = rtype, lang = 'en', count = 100)
+#    pretty_print(results)    
+    return results.get('statuses')
 
-print search_tweets()
+
+def save_tweets():
+    search_term = 'thedress OR blueandblack OR whiteandgold OR dressgate OR  dressgate2015'
+    tweets = search_tweets(search_term, 'mixed')
+#    pretty_print(tweets[0])
+    #text, id, favorite_count, retweeted, retweet_count, entities['hastags'][i]['text'], user['verified'], user['followers_count'], created_at , possibly_sensitive, 
+    with open('tweet_data.csv', 'w+') as csvfile:
+        writer = csv.writer(csvfile)
+        for t in tweets:
+            print t['text']
+            hashtags = [tag['text'] for tag in t['entities']['hashtags']]
+            text = t['text'].decode('unicode_escape').encode('ascii', 'ignore')
+            csvdata = [text, t['id'], t['favorite_count'], t['retweeted'],
+                       t['retweet_count'], t['user']['verified'], t['user']['followers_count'], 
+                       t['created_at'] , t['possibly_sensitive'], '|'.join(hashtags)]
+            writer.writerow(csvdata)
+        
+
+save_tweets()
